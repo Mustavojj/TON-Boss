e2e477e019136a0bfc75e87ceccfc5d9f3d4abaa
Mustavojj <mostavojj@gmail.com>
2025-11-24 23:34:25 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js
+export const SUPABASE_CONFIG = {
+  url: 'https://ztjokngpzbsuykwpcscz.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0am9rbmdwemJzdXlrd3Bjc2N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5ODAzNTIsImV4cCI6MjA3OTU1NjM1Mn0.jojOEgs3-MacLbT3i7wYoIiEkD4Z6K-Ym1EgBD1qVPs'
+};
+
+export const APP_CONFIG = {
+  name: 'TON BOSS',
+  version: '2.0.0',
+  minWithdraw: 0.10,
+  adValue: 10,
+  dailyAdLimit: 50,
+  conversionRate: 10000,
+  taskPricePer1k: 1 // 1 TON per 1000 members
+};
